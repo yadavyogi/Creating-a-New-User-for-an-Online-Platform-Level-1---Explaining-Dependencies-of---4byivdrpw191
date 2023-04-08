318cb04cb20a1323a2fab13cb22c619992aad08e
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-04-08 11:25:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,47 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 // Write POST endpoint for registering new user
+// app.post("/api/v1/details", (req, res) => {
+//   const { name, mail, number } = req.body;
+//   // if (name.trim().length === 0 || mail.trim().length === 0) {
+//   //   res.status(400).json({ message: "Invalid data" });
+//   // }
+//   // console.log(userDetails.length);
+//   const user = { id: userDetails.length + 1, name, mail, number };
+//   userDetails.push(user);
+//   // const data = fs.writeFileSync(
+//   //   `${__dirname}/data/userDetails.json`,
+//   //   JSON.stringify(userDetails),
+//   //   "utf8"
+//   // );
+//   res.status(201).json({
+//     status: "Success",
+//     message: "User registered successfully",
+//     data: {
+//       newProduct: userDetails[userDetails.length - 1],
+//     },
+//   });
+// });
+
+app.post("/api/v1/details", (req, res) => {
+  const newId = userDetails[userDetails.length - 1].id + 1;
+  const { name, mail, number } = req.body;
+  const newUser = { id: newId, name, mail, number };
+  userDetails.push(newUser);
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "User registered successfully",
+        data: {
+          userDetails: newUser,
+        },
+      });
+    }
+  );
+});
 
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
